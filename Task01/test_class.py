08429151ee208a1d267da7ea6491bb7130bafede
NikitaMakshev <nikita.makshev@mail.ru>
2023-03-16 11:30:09 +0300

    Complete

--- Task01/test_class.py
+++ Task01/test_class.py
@@ -1,31 +1,31 @@
 import pytest
-from triangle_class import Triangle
+from Triangle import Triangle
 from triangle_func import IncorrectTriangleSides
 
 
 class TestTriangleClass:
     def test_triangle_create(self):
-        triangle = Triangle(7, 7, 10)
-        assert triangle.a == 7
-        assert triangle.b == 7
-        assert triangle.c == 10
+        triangle = Triangle(10, 12, 14)
+        assert triangle.first_side == 10
+        assert triangle.second_side == 12
+        assert triangle.third_side == 14
 
-    def test_triangle_create_with_incorrect_type(self):
+    def test_triangle_create_with_value_error(self):
         with pytest.raises(IncorrectTriangleSides):
-            Triangle('1','7','1')
+            Triangle("a", "b", "c")
 
     def test_triangle_create_with_incorrect_sides(self):
         with pytest.raises(IncorrectTriangleSides):
-            Triangle(7, 5, 1)
+            Triangle(1, 2, 3)
 
-    def test_triangle_type_nonequilateral(self):
-        assert Triangle(7, 9, 11).triangle_type() == 'nonequilateral'
+    def test_triangle_method_type_nonequilateral(self):
+        assert Triangle(10, 12, 14).triangle_type() == 'nonequilateral'
 
-    def test_triangle_type_equilateral(self):
-        assert Triangle(7, 7, 7).triangle_type() == 'equilateral'
+    def test_triangle_method_type_equilateral(self):
+        assert Triangle(10, 10, 10).triangle_type() == 'equilateral'
 
-    def test_triangle_type_isosceles(self):
-        assert Triangle(7, 7, 10).triangle_type() == 'isosceles'
+    def test_triangle_method_type_isosceles(self):
+        assert Triangle(10, 10, 19).triangle_type() == 'isosceles'
 
-    def test_triangle_perimeter(self):
-        assert Triangle(7, 7, 10).perimeter() == 24
+    def test_triangle_method_perimeter(self):
+        assert Triangle(10, 12, 14).perimeter() == 36
--- Task01/triangle_func.py
+++ Task01/triangle_func.py
@@ -5,38 +5,39 @@ class IncorrectTriangleSides(Exception):
     pass
 
 
-def get_triangle_type(a: int, b: int, c: int):
+def get_triangle_type(first_side: int, second_side: int, third_side: int) -> str:
     """
     Function for checking type of triangle
-    :param a: first side of triangle (int)
-    :param b: second side of triangle (int)
-    :param c: third side of triangle (int)
+    :param first_side: first side of triangle (int)
+    :param second_side: second side of triangle (int)
+    :param third_side: third side of triangle (int)
     :return: type of triangle (str)
     :raises: IncorrectTriangleSides: if sides was set incorrectly
-    >>> get_triangle_type(7, 7, 10)
-    'isosceles'
-    >>> get_triangle_type(7, 7, 7)
+
+    >>> get_triangle_type(10, 10, 10)
     'equilateral'
-    >>> get_triangle_type(7, 9, 11)
+    >>> get_triangle_type(10, 12, 14)
     'nonequilateral'
-    >>> get_triangle_type('1','7','1')
+    >>> get_triangle_type(10, 10, 19)
+    'isosceles'
+    >>> get_triangle_type("a","b","c")
     Traceback (most recent call last):
      ...
-    IncorrectTriangleSides: Incorrect Triangle Sides
-    >>> get_triangle_type(7, 5, 1)
+    IncorrectTriangleSides: Sides set incorrectly
+    >>> get_triangle_type(1, 2, 3)
     Traceback (most recent call last):
      ...
-    IncorrectTriangleSides: Incorrect Triangle Sides
+    IncorrectTriangleSides: Sides set incorrectly
     """
 
-    if a + b <= c or a + c <= b or b + c <= a:
-        raise IncorrectTriangleSides("Incorrect Triangle Sides")
-    if not isinstance(a, int) or not isinstance(b, int) or not isinstance(c, int):
-        raise IncorrectTriangleSides("Incorrect Triangle Sides")
+    if (first_side + second_side <= third_side
+            or first_side + third_side <= second_side
+            or second_side + third_side <= first_side):
+        raise IncorrectTriangleSides('Sides set incorrectly')
 
-    if a != b != c:
+    if first_side != second_side != third_side:
         return "nonequilateral"
-    elif a == b == c:
+    elif first_side == second_side == third_side:
         return "equilateral"
     else:
         return "isosceles"
